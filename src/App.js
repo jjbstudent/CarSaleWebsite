fb18ec9b8c0dfe74d0efa8b9cc5c49b0b550ac73
Jermaine Bandoo <jblearn2023@gmail.com>
2024-10-07 20:20:39 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/homepage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import About from './components/about';
+import Footer from './components/footer';
 
 const App = () => {
   return (
-    <div>
-      <NavBar/>
-      <Homepage/>
-      
-    </div>
-  )
-}
-
+    <Router> {/* Wrap everything in Router */}
+      <NavBar />
+      <Routes> {/* Define the routes */}
+        <Route path="/" element={<Homepage />} /> {/* Route for Homepage */}
+        <Route path="/about" element={<About />} /> {/* Route for About Us */}
+        {/* Add other routes as needed */}
+      </Routes>
+      <Footer />  {/* Add Footer outside of Routes */}
+    </Router>
+  );
+};
 
 export default App;
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom'; // Import Link for routing
 
 function CarNavbar() {
   return (
@@ -11,9 +12,8 @@ function CarNavbar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/about">About Us</Nav.Link> {/* Link to About Us */}
             <Nav.Link href="#cars">Cars for Sale</Nav.Link>
-            <Nav.Link href="#financing">Financing Options</Nav.Link>
             <Nav.Link href="#sell">Sell Your Car</Nav.Link>
             <Nav.Link href="#reviews">Reviews</Nav.Link>
             <Nav.Link href="#contact">Contact Us</Nav.Link>
